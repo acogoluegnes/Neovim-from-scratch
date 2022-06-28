ad95d4e74bf40b3d62c736d190e9707ffb4b2cc6
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2022-06-28 09:43:03 +0200

    Adapt nvim tree new settings

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -25,7 +25,7 @@ nvim_tree.setup {
   open_on_tab = false,
   hijack_cursor = false,
   update_cwd = true,
-  update_to_buf_dir = {
+  hijack_directories = {
     enable = true,
     auto_open = true,
   },
@@ -61,7 +61,7 @@ nvim_tree.setup {
     height = 30,
     hide_root_folder = false,
     side = "left",
-    auto_resize = true,
+    adaptive_size = true,
     mappings = {
       custom_only = false,
       list = {
